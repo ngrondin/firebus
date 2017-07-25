642412849522f87142ea2385263d1d9b5283bc80
ngrondin <ngrondin78@gmail.com>
2017-07-26 08:33:12 +1000

    removed ssdp

--- FireBus/src/com/nic/utils/ssdp/SSDPClientListener.java
+++
@@ -1,9 +0,0 @@
-package com.nic.utils.ssdp;
-
-public interface SSDPClientListener
-{
-	public void registerServer(String host, int port, SSDPResponse response);
-	
-	public void deregisterServer(String host, int port, SSDPNotify notify);
-
-}
