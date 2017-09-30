e1c85451de0f14b4328c88f3b1a7aa79f7e47ef8
ngrondin <ngrondin78@gmail.com>
2017-09-30 20:36:11 +1000

    Contributions

--- FirebusAdaptersJava/src/com/nic/firebus/adapters/MongoDBAdatper.java
+++ FirebusAdaptersJava/src/com/nic/firebus/adapters/MongoDBAdatper.java
@@ -1,5 +1,7 @@
 package com.nic.firebus.adapters;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Iterator;
 
 import org.bson.Document;
@@ -7,25 +9,76 @@ import org.bson.Document;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.nic.firebus.Firebus;
+import com.nic.firebus.Payload;
+import com.nic.firebus.exceptions.FunctionErrorException;
+import com.nic.firebus.information.ServiceInformation;
+import com.nic.firebus.interfaces.Consumer;
+import com.nic.firebus.interfaces.ServiceProvider;
+import com.nic.firebus.utils.JSONObject;
 
-public class MongoDBAdatper
+public class MongoDBAdatper extends Adapter  implements ServiceProvider, Consumer
 {
-
-	public static void main(String[] args)
+	protected MongoClient client;
+	protected MongoDatabase database;
+	
+	public MongoDBAdatper(Firebus n, JSONObject c)
 	{
-		MongoClient mongoClient = new MongoClient();
-		MongoDatabase database = mongoClient.getDatabase("local");
-		for (String name : database.listCollectionNames()) 
+		super(n, c);
+		connectMongo();
+	}
+
+	protected void connectMongo()
+	{
+		if(client != null)
 		{
-		    System.out.println(name);
+			client.close();
+			client = null;
 		}
-		MongoCollection<Document> collection = database.getCollection("restapourants");
-		Iterator<Document> it = collection.find(new Document()).iterator();
-		while(it.hasNext())
+		client =  new MongoClient(config.getString("connectionstring"));
+		database = client.getDatabase(config.getString("database"));
+	}
+	
+
+	public void consume(Payload payload)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Payload service(Payload payload) throws FunctionErrorException
+	{
+		Payload response = new Payload();
+		try
 		{
-			Document doc = it.next();
-			System.out.println(doc);
+			JSONObject request = new JSONObject(payload.getString());
+			if(database != null)
+			{
+				MongoCollection<Document> collection = database.getCollection("po");
+				Iterator<Document> it = collection.find(Document.parse("{vendor:\"NicCo\"}")).iterator();
+				while(it.hasNext())
+				{
+					Document doc = it.next();
+					System.out.println(doc.toJson());
+				}
+			}
+			else
+			{
+				throw new FunctionErrorException("database object is null");
+			}
+			
 		}
-		mongoClient.close();
+		catch(Exception e)
+		{
+			throw new FunctionErrorException(e.getMessage());
+		}
+		
+		return response;
+	}
+
+	public ServiceInformation getServiceInformation()
+	{
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
