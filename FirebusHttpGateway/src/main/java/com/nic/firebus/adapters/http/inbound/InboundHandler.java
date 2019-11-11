8071e6683b474aba74d6eab04acf4b030a664ef6
Nicolas <ngrondin78@gmail.com>
2019-11-11 19:56:46 +1100

    Added the OPTIONS handling on tne inbound handler

--- FirebusHttpGateway/src/main/java/com/nic/firebus/adapters/http/inbound/InboundHandler.java
+++ FirebusHttpGateway/src/main/java/com/nic/firebus/adapters/http/inbound/InboundHandler.java
@@ -20,12 +20,14 @@ public abstract class InboundHandler extends Handler
 
 	private Firebus firebus;
 	private String service;
+	private int timeout;
 	
 	public InboundHandler(DataMap c, Firebus f) 
 	{
 		super(c);
 		firebus = f;
 		service = handlerConfig.getString("service");
+		timeout = handlerConfig.containsKey("timeout") ? handlerConfig.getNumber("timeout").intValue() : 10000;
 	}
 	
 	
@@ -33,54 +35,67 @@ public abstract class InboundHandler extends Handler
 	{
 		String token = null;
 		String accept = req.getHeader("accept");
-		if(handlerConfig.containsKey("authentication")  &&  (token = getToken(req)) == null)
+		if(req.getMethod().equals("OPTIONS"))
 		{
-			DataMap authConfig = handlerConfig.getObject("authentication");
-			String loginUrl = authConfig.getString("loginurl");
-			if(loginUrl != null)
-			{
-				String currentUrl = req.getRequestURL().toString();
-				String path = req.getRequestURI();
-				String host = currentUrl.substring(0, (currentUrl.length() - path.length()));
-				String nonce = "12345";
-				String resolvedLoginUrl = loginUrl.replace("${host}", host).replace("${path}", path).replace("${currenturl}", currentUrl).replace("${nonce}", nonce);
-				resp.setStatus(HttpServletResponse.SC_TEMPORARY_REDIRECT);
-				resp.setHeader("Location", resolvedLoginUrl);
-			}
-			else
-			{
-				resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-		        PrintWriter writer = resp.getWriter();
-		        writer.println("<html><title>401</title><body>Unauthorised</body></html>");
-			}	
+			resp.setStatus(HttpServletResponse.SC_NO_CONTENT);
+			resp.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+			resp.setHeader("Access-Control-Allow-Headers", "Content-Type");
+			resp.setHeader("Access-Control-Allow-Origin", req.getHeader("Origin"));
+			resp.setHeader("Access-Control-Allow-Credentials", "true");
 		}
 		else
 		{
-			try 
+			if(handlerConfig.containsKey("authentication")  &&  (token = getToken(req)) == null)
 			{
-				Payload fbReq = processRequest(req);
-				if(fbReq != null)
+				DataMap authConfig = handlerConfig.getObject("authentication");
+				String loginUrl = authConfig.getString("loginurl");
+				if(loginUrl != null)
 				{
-					if(token != null)
-						fbReq.metadata.put("token", token);
-					Payload fbResp = firebus.requestService(service, fbReq);
-					processResponse(resp, fbResp);
+					String currentUrl = req.getRequestURL().toString();
+					String path = req.getRequestURI();
+					String host = currentUrl.substring(0, (currentUrl.length() - path.length()));
+					String nonce = "12345";
+					String resolvedLoginUrl = loginUrl.replace("${host}", host).replace("${path}", path).replace("${currenturl}", currentUrl).replace("${nonce}", nonce);
+					resp.setStatus(HttpServletResponse.SC_TEMPORARY_REDIRECT);
+					resp.setHeader("Location", resolvedLoginUrl);
 				}
 				else
 				{
+					resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+			        PrintWriter writer = resp.getWriter();
+			        writer.println("<html><title>401</title><body>Unauthorised</body></html>");
+				}	
+			}
+			else
+			{
+				try 
+				{
+					Payload fbReq = processRequest(req);
+					if(fbReq != null)
+					{
+						if(token != null)
+							fbReq.metadata.put("token", token);
+						Payload fbResp = firebus.requestService(service, fbReq, timeout);
+						resp.setHeader("Access-Control-Allow-Origin", req.getHeader("Origin"));
+						resp.setHeader("Access-Control-Allow-Credentials", "true");
+						processResponse(resp, fbResp);
+					}
+					else
+					{
+						resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+				        PrintWriter writer = resp.getWriter();
+				        writer.println("<html><title>500</title><body>Inbound process failed</body></html>");
+					}
+				} 
+				catch (Exception e)
+				{
 					resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
 			        PrintWriter writer = resp.getWriter();
-			        writer.println("<html><title>500</title><body>Inbound process failed</body></html>");
+					if(accept.contains("application/json"))
+						writer.println("{\r\n\t\"error\" : \"" + e.getMessage().replaceAll("\"", "'").replaceAll("\r", "\\\\r").replaceAll("\n", "\\\\n") + "\"\r\n}");
+					if(accept.contains("text/html"))
+						writer.println("<div>" + e.getMessage() + "</div>");
 				}
-			} 
-			catch (Exception e)
-			{
-				resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-		        PrintWriter writer = resp.getWriter();
-				if(accept.contains("application/json"))
-					writer.println("{\r\n\t\"error\" : \"" + e.getMessage().replaceAll("\"", "'").replaceAll("\r", "\\\\r").replaceAll("\n", "\\\\n") + "\"\r\n}");
-				if(accept.contains("text/html"))
-					writer.println("<div>" + e.getMessage() + "</div>");
 			}
 		}
 	}	
