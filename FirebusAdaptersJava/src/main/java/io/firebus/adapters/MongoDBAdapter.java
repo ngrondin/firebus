f0efa295d380a2e6c340daf73c66ca904b012f35
Nicolas <ngrondin78@gmail.com>
2020-03-29 13:45:02 +1100

    Correction on the mongodb adapter

--- FirebusAdaptersJava/src/main/java/io/firebus/adapters/MongoDBAdapter.java
+++ FirebusAdaptersJava/src/main/java/io/firebus/adapters/MongoDBAdapter.java
@@ -15,6 +15,7 @@ import io.firebus.exceptions.FunctionErrorException;
 import io.firebus.information.ServiceInformation;
 import io.firebus.interfaces.Consumer;
 import io.firebus.interfaces.ServiceProvider;
+import io.firebus.utils.DataException;
 import io.firebus.utils.DataList;
 import io.firebus.utils.DataMap;
 
@@ -48,66 +49,13 @@ public class MongoDBAdapter extends Adapter  implements ServiceProvider, Consume
 	{
 		try
 		{
-			DataMap packet = new DataMap(payload.getString());
-			String objectName = packet.getString("object");
-			String operation = packet.getString("operation");
-			DataMap data = packet.getObject("data");
-			DataMap key = packet.getObject("key");
-			if(database != null)
-			{
-				MongoCollection<Document> collection = database.getCollection(objectName);
-				if(collection != null)
-				{
-					if(key != null)
-					{
-						Document findDoc = Document.parse(key.toString());
-						Document existingDoc = collection.find(findDoc).first();
-												
-						if(existingDoc != null)
-						{
-							if(data != null)
-							{
-								Document incomingDoc = Document.parse(data.toString());
-								if(operation == null || (operation != null && operation.equals("insert")) || (operation !=null && operation.equals("update")))
-									collection.updateOne(existingDoc, new Document("$set", incomingDoc));
-								else if(operation != null  && operation.equals("replace"))
-									collection.replaceOne(existingDoc, incomingDoc);
-								else if(operation != null  && operation.equals("delete"))
-									collection.deleteOne(existingDoc);
-							}
-							else
-							{
-								if(operation != null  && operation.equals("delete"))
-									collection.deleteOne(existingDoc);
-							}
-						}
-						else
-						{
-							if(data != null)
-								data.merge(key);
-							else
-								data = key;
-							Document incomingDoc = Document.parse(data.toString());
-							if(operation == null || (operation !=null && operation.equals("insert")) || (operation !=null && operation.equals("update")) || (operation !=null && operation.equals("replace")))
-								collection.insertOne(incomingDoc);
-						}
-					}
-				}
-				else
-				{
-					logger.severe("No collection exists by this namel");
-				}
-			}
-			else
-			{
-				logger.severe("Database as not been specificied in the configuration");
-			}			
+			DataMap request = new DataMap(payload.getString());
+			upsert(request);
 		}
-		catch(Exception e)
+		catch(DataException e)
 		{
-			logger.severe(e.getMessage());
-		}		
-		
+			logger.severe("Error consuming data : " + e.getMessage());
+		}
 	}
 
 	public Payload service(Payload payload) throws FunctionErrorException
@@ -118,64 +66,138 @@ public class MongoDBAdapter extends Adapter  implements ServiceProvider, Consume
 		{
 			logger.finer("Starting mongo request");
 			DataMap request = new DataMap(payload.getString());
-			String objectName = request.getString("object");
-			int page = request.containsKey("page") ? request.getNumber("page").intValue() : 0;
-			if(database != null)
+			if(request.containsKey("filter")) 
 			{
-				MongoCollection<Document> collection = database.getCollection(objectName);
-				if(collection != null)
+				DataList list = get(request);
+				responseJSON.put("result", list);
+			}
+			else if(request.containsKey("key"))
+			{
+				upsert(request);
+				responseJSON.put("result", "ok");
+			}
+			response = new Payload(null, responseJSON.toString().getBytes());
+
+		}
+		catch(Exception e)
+		{
+			logger.severe("Error processing data request : " + e.getMessage());
+			throw new FunctionErrorException(e.getMessage());
+		}		
+		return response;
+	}
+	
+	private DataList get(DataMap request) throws FunctionErrorException, DataException
+	{
+		DataList responseList = null;
+		String objectName = request.getString("object");
+		int page = request.containsKey("page") ? request.getNumber("page").intValue() : 0;
+		if(database != null)
+		{
+			MongoCollection<Document> collection = database.getCollection(objectName);
+			if(collection != null)
+			{
+				Iterator<Document> it = null;
+				if(request.containsKey("filter"))
 				{
-					Iterator<Document> it = null;
-					if(request.containsKey("filter"))
-					{
-						DataMap filter = request.getObject("filter");
-						Document filterDoc = Document.parse(filter.toString()); 
-						it = collection.find(filterDoc).iterator();		
-					}
-					else if(request.containsKey("aggregation"))
-					{
-						DataList aggregation = request.getList("aggregation");
-						ArrayList<Document> list = new ArrayList<Document>();
-						for(int i = 0; i < aggregation.size(); i++)
-							list.add(Document.parse(aggregation.getObject(i).toString()));
-						it = collection.aggregate(list).iterator();
-					}
-					else
-					{
-						it = collection.find(new Document()).iterator();		
-					}
-					
-					if(it != null)
-					{
-						DataList list = new DataList();
-						for(int i = 0; it.hasNext() && i < (page * pageSize); i++)
-							it.next();
-						while(it.hasNext() && list.size() < pageSize)
-						{
-							Document doc = it.next();
-							String str = doc.toJson();
-							DataMap obj = new DataMap(str);
-							list.add(obj);
-						}
-						responseJSON.put("result", list);
-						response.setData(responseJSON.toString());
-					}
+					DataMap filter = request.getObject("filter");
+					Document filterDoc = Document.parse(filter.toString()); 
+					it = collection.find(filterDoc).iterator();		
+				}
+				else if(request.containsKey("aggregation"))
+				{
+					DataList aggregation = request.getList("aggregation");
+					ArrayList<Document> list = new ArrayList<Document>();
+					for(int i = 0; i < aggregation.size(); i++)
+						list.add(Document.parse(aggregation.getObject(i).toString()));
+					it = collection.aggregate(list).iterator();
 				}
 				else
 				{
-					throw new FunctionErrorException("No collection exists by this namel");
+					it = collection.find(new Document()).iterator();		
+				}
+				
+				if(it != null)
+				{
+					responseList = new DataList();
+					for(int i = 0; it.hasNext() && i < (page * pageSize); i++)
+						it.next();
+					while(it.hasNext() && responseList.size() < pageSize)
+					{
+						Document doc = it.next();
+						String str = doc.toJson();
+						DataMap obj = new DataMap(str);
+						responseList.add(obj);
+					}
 				}
 			}
 			else
 			{
-				throw new FunctionErrorException("Database as not been specificied in the configuration");
-			}			
+				throw new FunctionErrorException("No collection exists by this namel");
+			}
 		}
-		catch(Exception e)
+		else
 		{
-			throw new FunctionErrorException(e.getMessage());
-		}		
-		return response;
+			throw new FunctionErrorException("Database as not been specificied in the configuration");
+		}	
+		return responseList;
+	}
+	
+	private void upsert(DataMap packet)
+	{
+		String objectName = packet.getString("object");
+		String operation = packet.getString("operation");
+		DataMap data = packet.getObject("data");
+		DataMap key = packet.getObject("key");
+		if(database != null)
+		{
+			MongoCollection<Document> collection = database.getCollection(objectName);
+			if(collection != null)
+			{
+				if(key != null)
+				{
+					Document findDoc = Document.parse(key.toString());
+					Document existingDoc = collection.find(findDoc).first();
+											
+					if(existingDoc != null)
+					{
+						if(data != null)
+						{
+							Document incomingDoc = Document.parse(data.toString());
+							if(operation == null || (operation != null && operation.equals("insert")) || (operation !=null && operation.equals("update")))
+								collection.updateOne(existingDoc, new Document("$set", incomingDoc));
+							else if(operation != null  && operation.equals("replace"))
+								collection.replaceOne(existingDoc, incomingDoc);
+							else if(operation != null  && operation.equals("delete"))
+								collection.deleteOne(existingDoc);
+						}
+						else
+						{
+							if(operation != null  && operation.equals("delete"))
+								collection.deleteOne(existingDoc);
+						}
+					}
+					else
+					{
+						if(data != null)
+							data.merge(key);
+						else
+							data = key;
+						Document incomingDoc = Document.parse(data.toString());
+						if(operation == null || (operation !=null && operation.equals("insert")) || (operation !=null && operation.equals("update")) || (operation !=null && operation.equals("replace")))
+							collection.insertOne(incomingDoc);
+					}
+				}
+			}
+			else
+			{
+				logger.severe("No collection exists by this namel");
+			}
+		}
+		else
+		{
+			logger.severe("Database as not been specificied in the configuration");
+		}
 	}
 
 	public ServiceInformation getServiceInformation()
