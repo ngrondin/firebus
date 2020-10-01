556ed2ad95bd3f0fe86eb953151deff2dc817c4e
Nicolas <ngrondin78@gmail.com>
2020-10-01 13:40:08 +1000

    Added forms on generaloutbound handler

--- FirebusHttpGateway/src/main/java/io/firebus/adapters/http/outbound/GeneralOutboundHandler.java
+++ FirebusHttpGateway/src/main/java/io/firebus/adapters/http/outbound/GeneralOutboundHandler.java
@@ -1,15 +1,20 @@
 package io.firebus.adapters.http.outbound;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import javax.servlet.ServletException;
 
 import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
 import io.firebus.Firebus;
@@ -33,13 +38,26 @@ public class GeneralOutboundHandler extends OutboundHandler {
 		HttpUriRequest httpRequest = null;
 		if(method.equals("post")) 
 		{
-			httpRequest = new HttpPost(url);
-			((HttpPost)httpRequest).setEntity(new ByteArrayEntity(request.get("body").toString().getBytes()));
+			HttpPost httpPost = new HttpPost(url);
+			if(request.containsKey("body")) {
+				httpPost.setEntity(new ByteArrayEntity(request.get("body").toString().getBytes()));
+			} else if(request.containsKey("form")) {
+				List<NameValuePair> formParams = new ArrayList<NameValuePair>();
+				Iterator<String> it = request.getObject("form").keySet().iterator();
+				while(it.hasNext()) {
+					String key = it.next();
+					formParams.add(new BasicNameValuePair(key, request.getObject("form").getString(key)));
+				}				
+				httpPost.setEntity(new UrlEncodedFormEntity(formParams, "UTF-8"));
+			}
+			httpPost.setHeader("Content-Type", "application/x-www-form-urlencoded");
+			httpRequest = httpPost;
 		}
 		else if(method.equals("get"))
 		{
 			httpRequest = new HttpGet(url);
 		}
+		
 		if(request.containsKey("cookie")) {
 			String cookie = "";
 			Iterator<String> it = request.getObject("cookie").keySet().iterator();
@@ -52,6 +70,13 @@ public class GeneralOutboundHandler extends OutboundHandler {
 		if(request.containsKey("authorization")) {
 			String authorization = request.getString("authorization");
 			httpRequest.setHeader("Authorization", authorization);
+		}
+		if(request.containsKey("headers")) {
+			Iterator<String> it = request.getObject("headers").keySet().iterator();
+			while(it.hasNext()) {
+				String key = it.next();
+				httpRequest.setHeader(key, request.getObject("headers").getString(key));
+			}
 		}
 		return httpRequest;
 	}
