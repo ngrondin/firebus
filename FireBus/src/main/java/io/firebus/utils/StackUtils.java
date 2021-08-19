cf4f66907a8b883ecd82754f5e27d30e7c522038
Nicolas <ngrondin78@gmail.com>
2021-08-19 17:56:12 +1000

    Fix on StackUtils

--- FireBus/src/main/java/io/firebus/utils/StackUtils.java
+++ FireBus/src/main/java/io/firebus/utils/StackUtils.java
@@ -1,28 +1,33 @@
 package io.firebus.utils;
 
+import java.util.Arrays;
+
 public class StackUtils {
 	
 	public static String getStackString() {
 		StackTraceElement[] elements = Thread.getAllStackTraces().get(Thread.currentThread());
-		boolean started = false;
+		int start = 0;
+		while(!elements[start].getMethodName().equals("getStackString"))
+			start++;
+		StackTraceElement[] output = Arrays.copyOfRange(elements, start + 1, elements.length);
+		return toString(output);
+	}
+	
+	
+	public static String toString(StackTraceElement[] elements) {
 		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < elements.length; i++) {
-			if(started) {
-				if(sb.length() > 0) sb.append("\r\n");
-				sb.append(elements[i].getClassName());
-				sb.append(".");
-				sb.append(elements[i].getMethodName());
-				sb.append("(");
-				sb.append(elements[i].getFileName());
-				sb.append(":");
-				sb.append(elements[i].getLineNumber());
-				sb.append(")");
-			} else {
-				if(elements[i].getMethodName().equals("getStackString")) 
-					started = true;
-			}
+			if(sb.length() > 0) sb.append("\r\n");
+			sb.append(elements[i].getClassName());
+			sb.append(".");
+			sb.append(elements[i].getMethodName());
+			sb.append("(");
+			sb.append(elements[i].getFileName());
+			sb.append(":");
+			sb.append(elements[i].getLineNumber());
+			sb.append(")");
 		}
-		return sb.toString();
+		return sb.toString();		
 	}
 
 }
