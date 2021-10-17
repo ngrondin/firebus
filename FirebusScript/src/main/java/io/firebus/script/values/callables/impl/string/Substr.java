55d0ca2cf4913a637e61c820d7e74ccb498aa9fb
Nicolas <ngrondin78@gmail.com>
2021-10-18 10:27:19 +1100

    Fix on String.substr

--- FirebusScript/src/main/java/io/firebus/script/values/callables/impl/string/Substr.java
+++ FirebusScript/src/main/java/io/firebus/script/values/callables/impl/string/Substr.java
@@ -15,20 +15,29 @@ public class Substr extends StringFunction {
 	public SValue call(SValue... arguments) throws ScriptCallException {
 		if(arguments.length > 0) {
 			SValue bv = arguments[0];
+			String str = string.getString();
+			int len = str.length();
 			if(bv instanceof SNumber) {
 				int b = ((SNumber)bv).getNumber().intValue();
-				if(b < 0) b = string.getString().length() + b;
-				if(arguments.length >= 2) {
-					SValue lv = arguments[1];
-					if(lv instanceof SNumber) {
-						int l = ((SNumber)lv).getNumber().intValue();
-						return new SString(string.getString().substring(b, b + l));
-					} else {
-						throw new ScriptCallException("substr requies number arguments");
-					}
+				if(b < 0) b = len + b;
+				if(b < 0) b = 0;
+				if(b > len) {
+					return new SString("");
 				} else {
-					return new SString(string.getString().substring(b));
+					if(arguments.length >= 2) {
+						SValue lv = arguments[1];
+						if(lv instanceof SNumber) {
+							int l = ((SNumber)lv).getNumber().intValue();
+							if(b + l > len) l = len - b;
+							return new SString(str.substring(b, b + l));
+						} else {
+							throw new ScriptCallException("substr requies number arguments");
+						}
+					} else {
+						return new SString(string.getString().substring(b));
+					}					
 				}
+
 			} else {
 				throw new ScriptCallException("substr requies number arguments");
 			}
